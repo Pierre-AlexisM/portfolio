03317bccbf65f2272c4f1d068adb26701bae4a33
unknown <pierre-alexis.maerten@getic.net>
2020-05-04 17:15:25 +0200

    lol

--- src/JS/burgerMenu.js
+++ src/JS/burgerMenu.js
@@ -1,6 +1,11 @@
 const nav = document.getElementById('navBar');
 const burgerIcon = document.getElementById('burgerIcon');
+const logo = document.getElementById('logo');
 
 burgerIcon.addEventListener('click', function() {
     nav.classList.toggle('is-open')
 });
+
+if (nav.classList('is-open')) {
+    logo.classList.toggle('is-open')
+}
