766d697a75a60fd203ed759ef6e6e92ffc2c632b
unknown <pierre-alexis.maerten@getic.net>
2020-05-14 16:30:44 +0200

    work section

---
+++ src/JS/sketch.js
@@ -0,0 +1,98 @@
+var streams = [];
+var fadeInterval = 1.6;
+var symbolSize = 24;
+
+function setup() {
+  createCanvas(
+    500, 500
+  );
+  background(0);
+
+  var x = 0;
+  for (var i = 0; i <= width / symbolSize; i++) {
+    var stream = new Stream();
+    stream.generateSymbols(x, random(-2000, 0));
+    streams.push(stream);
+    x += symbolSize
+  }
+
+  textFont('Consolas');
+  textSize(symbolSize);
+}
+
+function draw() {
+  background(20, 21, 28);
+  streams.forEach(function(stream) {
+    stream.render();
+  });
+}
+
+function Symbol(x, y, speed, first, opacity) {
+  this.x = x;
+  this.y = y;
+  this.value;
+
+  this.speed = speed;
+  this.first = first;
+  this.opacity = opacity;
+
+  this.switchInterval = round(random(10, 20));
+
+  this.setToRandomSymbol = function() {
+    var charType = round(random(0, 5));
+    if (frameCount % this.switchInterval == 0) {
+      if (charType > 1) {
+        // set it to Katakana
+        this.value = String.fromCharCode(
+          0x30A0 + floor(random(0, 97))
+        );
+      } else {
+        // set it to numeric
+        this.value = floor(random(0,10));
+      }
+    }
+  }
+
+  this.rain = function() {
+    this.y = (this.y >= height) ? 0 : this.y += this.speed;
+  }
+
+}
+
+function Stream() {
+  this.symbols = [];
+  this.totalSymbols = round(random(5, 35));
+  this.speed = random(4, 18);
+
+  this.generateSymbols = function(x, y) {
+    var opacity = 255;
+    var first = round(random(0, 4)) == 1;
+    for (var i =0; i <= this.totalSymbols; i++) {
+      symbol = new Symbol(
+        x,
+        y,
+        this.speed,
+        first,
+        opacity
+      );
+      symbol.setToRandomSymbol();
+      this.symbols.push(symbol);
+      opacity -= (255 / this.totalSymbols) / fadeInterval;
+      y -= symbolSize;
+      first = false;
+    }
+  }
+
+  this.render = function() {
+    this.symbols.forEach(function(symbol) {
+      if (symbol.first) {
+        fill(255, 215, 25, symbol.opacity);
+      } else {
+        fill(205, 133, 12, symbol.opacity);
+      }
+      text(symbol.value, symbol.x, symbol.y);
+      symbol.rain();
+      symbol.setToRandomSymbol();
+    });
+  }
+}
